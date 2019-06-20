f9a7c29693d8b771fa2cf4c79ea5b44dffedd3f7
denF0x <fox-94.09@yandex.ru>
2019-06-20 16:30:29 +0300

    Denis Babkin homework

--- closure-typing/calculator.js
+++ closure-typing/calculator.js
@@ -1,5 +1,31 @@
-function calculator() {
-  // your code here
+function calculator(str) {
+    var operator = '';
+    var hereLiesOperator;
+    var result;
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
+            operator = str[i];
+            hereLiesOperator = i;
+        }
+    }
+    var first = +str.substring(0, hereLiesOperator);
+    var second = +str.substring(hereLiesOperator + 1, str.length);
+    switch (operator) {
+        case '+':
+            result = first + second;
+            return result;
+        case '-':
+            result = first - second;
+            return result;
+        case '*':
+            result = first * second;
+            return result;
+        case '/':
+            result = first / second;
+            return result;
+    }
+    return result;
+
 }
 
 module.exports = calculator;
--- closure-typing/kebabToCamelCase.js
+++ closure-typing/kebabToCamelCase.js
@@ -1,5 +1,17 @@
-function kebabToCamelCase() {
-  // your code here
+function kebabToCamelCase(str) {
+    var char;
+    var newString = "";
+
+    for (var i = 0; i < str.length; i++) {
+        char = str[i];
+        if (char == '-') {
+            char = str[i + 1].toUpperCase();
+            i++;
+        }
+        newString = newString + char;
+    }
+    return newString;
+
 }
 
 module.exports = kebabToCamelCase;
