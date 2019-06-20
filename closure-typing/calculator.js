a4114b90915071c77b47db4fb6502e25462a47d0
kseniia-v <x27kadis@gmail.com>
2019-06-21 02:27:45 +0300

    kseniia-v hw1

--- closure-typing/calculator.js
+++ closure-typing/calculator.js
@@ -1,5 +1,54 @@
 function calculator() {
-  // your code here
+  let a = arguments[0], result = 0, a1='', a2='', sign='';
+  for (let i = 0; i < a.length; i++){
+      if((a.charAt(i) > 0 || +a.charAt(i) === 0) && sign === ''){
+        a1 += a.charAt(i);
+      }
+      else if (sign === '') {
+        sign = a.charAt(i);
+      }
+      else{
+        a2 += a.charAt(i);
+      }
+  }
+  switch(sign){
+      case '+': result = +a1 + +a2; break;
+      case '-': result = a1 - a2; break;
+      case '*': result = a1 * a2; break;
+      case '/': result = a1 / a2; break;
+  }
+  return result;
 }
+/*
+//more beautiful
+function calculator() {
+    let a = arguments[0], sign='', placeIndex = 0, a1='', a2='', result = 0;
+
+    if (a.indexOf('+')>0){sign = '+';}
+      else
+    if (a.indexOf('-')>0) {sign = '-';}
+      else
+    if (a.indexOf('*')>0) {sign = '*';}
+      else
+    if (a.indexOf('/')>0) {sign = '/';}
+
+    placeIndex = a.indexOf(sign);
+
+    a1 = a.slice(0, placeIndex);
+    a2 = a.slice(placeIndex + 1, a.length);
+
+    switch(sign){
+        case '+': result = +a1 + +a2; break;
+        case '-': result = a1 - a2; break;
+        case '*': result = a1 * a2; break;
+        case '/': result = a1 / a2; break;
+    }
+
+    return result;
+
+}
+
+*/
+
 
 module.exports = calculator;
--- closure-typing/storage.js
+++ closure-typing/storage.js
@@ -1,5 +1,19 @@
 function storage() {
-
+  let container = {};
+  return function put(key,value){
+    if(value){
+      container[key] = value;
+    }else if (key && ! value) {
+      for (k in container){
+        if (k === key){return container[key];break}
+      }
+      return 'Not found';
+    }else{
+      return container;
+    };
+    put.clear = function(){container= {}; return container;}
+  }
 }
+
 
 module.exports = storage;
