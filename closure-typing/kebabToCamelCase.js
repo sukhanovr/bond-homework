dfed53afb36a90a215cb6b540f37bdadbb39b4eb
Oleh Babkov <babkoff1989@gmail.com>
2019-06-20 17:00:44 +0300

    Message 1

--- closure-typing/kebabToCamelCase.js
+++ closure-typing/kebabToCamelCase.js
@@ -1,5 +1,16 @@
-function kebabToCamelCase() {
-  // your code here
+function kebabToCamelCase(userInput) {
+  var char;
+  var newString = "";  
+
+  for (let i = 0; i < userInput.length; i++) {
+    char = userInput[i];
+    if (char == '-') {
+      char = userInput[i + 1].toUpperCase();
+      i++;
+    }
+    newString = newString + char;
+  }
+  return newString;
 }
 
 module.exports = kebabToCamelCase;
