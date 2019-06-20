30a48933bc870cbc45c7de86dd8d956f88d06946
Yelyzaveta Bitiukova <Yelyzaveta.Bitiukova@teaminternational.com>
2019-06-21 02:15:19 +0300

    Woks has been done by LizaB

--- closure-typing/calculator.js
+++ closure-typing/calculator.js
@@ -1,5 +1,42 @@
-function calculator() {
-  // your code here
+function calculator(str) {
+  var mathFuncList = {
+    '+' : (a, b) => {return +a + +b},
+    '-' : (a, b) => {return a - b},
+    '*' : (a, b) => {return a * b},
+    '/' : (a, b) => {return a / b}
+  }
+  arr = str.replace(/([+\-*/])/g, ' $1 ').split(' ')
+
+  while (arr.length > 1){
+    i = arr.indexOf('*')
+    i < 0 ? i = arr.indexOf('/') : i
+    i < 0 ? i = arr.indexOf('+') : i
+    i < 0 ? i = arr.indexOf('-') : i
+    arr[i-1] = mathFuncList[arr[i]](arr[i-1], arr[i+1])
+    arr.splice(i, 2)
+  }
+  return arr[0]
 }
 
 module.exports = calculator;
+
+
+
+console.log(calculator('3+2+4*4'))
+
+
+
+
+
+
+
+
+/*
+    '+' : function(a, b) {return BigInt(a + b)},
+    '-' : function(a, b) {return BigInt(a - b)},
+    '*' : function(a, b) {return BigInt(a * b)},
+    '/' : function(a, b) {return BigInt(a / b)}
+  }
+
+
+*/
