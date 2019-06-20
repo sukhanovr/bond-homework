f2f54c6d6dc45a51552aaba91f4dedc456be0ac2
shcherbinina-yelyzaveta <scherbininaliza@gmail.com>
2019-06-20 13:39:44 +0300

    homework #1 done

--- closure-typing/calculator.js
+++ closure-typing/calculator.js
@@ -1,5 +1,38 @@
-function calculator() {
-  // your code here
+function calculator(inputStr) {
+    var posOfOperator = 0;
+    var operator, firstOperand, secondOperand, result;
+    if(inputStr){
+        if(~inputStr.indexOf("+"))
+            posOfOperator = inputStr.indexOf("+");
+        else if(~inputStr.indexOf("-"))
+            posOfOperator = inputStr.indexOf("-");
+        else if(~inputStr.indexOf("*"))
+            posOfOperator = inputStr.indexOf("*");
+        else if(~inputStr.indexOf("/"))
+            posOfOperator = inputStr.indexOf("/");
+        if(posOfOperator){
+            operator = inputStr[posOfOperator];
+            firstOperand = +inputStr.substring(0,posOfOperator);
+            secondOperand = +inputStr.substring(posOfOperator+1);
+            if(firstOperand && secondOperand){
+                switch (operator) {
+                    case '+':
+                        result = firstOperand + secondOperand;
+                        break;
+                    case '-':
+                        result = firstOperand - secondOperand;
+                        break;
+                    case '*':
+                        result = firstOperand * secondOperand;
+                        break;
+                    case '/':
+                        result = firstOperand / secondOperand;
+                        break;
+                }
+            }
+        }
+        return result;
+    }
 }
 
 module.exports = calculator;
--- closure-typing/kebabToCamelCase.js
+++ closure-typing/kebabToCamelCase.js
@@ -1,5 +1,23 @@
-function kebabToCamelCase() {
-  // your code here
+function kebabToCamelCase(inputStr) {
+    var posOfHyphen, pos = 0;
+    var result = "";
+    if(inputStr){
+        if(~inputStr.indexOf("-", pos)) {
+            while (true) {
+                posOfHyphen = inputStr.indexOf("-", pos);
+                if(posOfHyphen === -1) {
+                    result+= inputStr.substring(pos);
+                    break;
+                }
+                result += inputStr.substring(pos, posOfHyphen);
+                result += inputStr[posOfHyphen+1].toUpperCase();
+                pos = posOfHyphen+2;
+            }
+            return result;
+        } else {
+            return inputStr;
+        }
+    }
 }
 
 module.exports = kebabToCamelCase;
