0fea529a7e52dcaca17f5c8124a424e64e1f394d
GrinUA <uvarov.yura@gmail.com>
2019-06-20 12:00:36 +0300

    Add files via upload

--- closure-typing/calculator.js
+++ closure-typing/calculator.js
@@ -1,5 +1,13 @@
-function calculator() {
-  // your code here
+function calculator(inputString) {
+    var first = parseInt(inputString);
+    var lengthOfFirst = first.toString().length;
+    var second = +inputString.substring(lengthOfFirst + 1);
+    var sign = inputString.substring(first.toString().length, first.toString().length + 1);
+    switch (sign) {
+        case "-" : return first - second;
+        case "+" : return  first + second;
+        case  "/" : return  first / second;
+        case  "*" : return  first * second;
+    }
 }
-
 module.exports = calculator;
--- closure-typing/kebabToCamelCase.js
+++ closure-typing/kebabToCamelCase.js
@@ -1,5 +1,14 @@
-function kebabToCamelCase() {
-  // your code here
+function kebabToCamelCase(inputString) {
+    var result = "";
+    var temp;
+  for (var i = 0; i < inputString.length; i++){
+      temp = inputString[i];
+      if (temp === "-"){
+          temp = inputString[i + 1].toUpperCase();
+          i++
+      }
+      result = result + temp;
+  }
+  return result
 }
-
 module.exports = kebabToCamelCase;
