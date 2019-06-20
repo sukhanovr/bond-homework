6c8d27938ce03e834a4909e0ba6fb2e442a5cd9c
Denis <den_85@ukr.net>
2019-06-20 12:05:01 +0300

    Add files via upload

--- closure-typing/calculator.js
+++ closure-typing/calculator.js
@@ -1,5 +1,43 @@
-function calculator() {
-  // your code here
+function calculator(e) {
+    // your code here
+
+    let value = 0;
+    let action = '';
+    let result = 0;
+    let index = 0;
+
+    for(let i=0; i< e.length; i++){
+
+        if( '+-/*'.includes(e[i]) || i === e.length-1){
+
+            if (result === 0) {
+                result = e.substring(0, e.indexOf(e[i]));
+                action = e[i];
+                index = e.indexOf(e[i]) + 1;
+                continue;
+            }
+
+            value = e.substring(index, e.indexOf(action, index) === -1 ? e.length : e.indexOf(action, index));
+            index = e.indexOf(action, index) + 1;
+
+            switch(action){
+                case '+':
+                    result = +result + +value;
+                    break;
+                case '-':
+                    result -= value;
+                    break;
+                case '/':
+                    result /= value;
+                    break;
+                case '*':
+                    result *= value;
+                    break;
+            }
+            action = e[i];
+        }
+    }
+    return result;
 }
 
 module.exports = calculator;
--- closure-typing/storage.js
+++ closure-typing/storage.js
@@ -1,5 +1,24 @@
-function storage() {
+function storage ( ) {
+    let data= {};
 
+     function store (paramName, paramValue) {
+
+         switch (arguments.length) {
+             case 2:
+                 data[paramName] = paramValue;
+                 break;
+             case 1:
+                 return ((paramName in data)? data[paramName] : "Not found");
+             default:
+                 return data;
+         }
+    }
+
+    store.clear = function(){
+        data = {};
+    }
+
+    return store;
 }
 
 module.exports = storage;
