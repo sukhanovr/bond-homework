da9b6237223a7e390585afda9d05f79e39e6cd72
Kateryna Mihal <Kateryna.Mihal@teaminternational.com>
2019-06-21 12:37:51 +0300

    commit kebab

--- closure-typing/kebabToCamelCase.js
+++ closure-typing/kebabToCamelCase.js
@@ -1,5 +1,20 @@
-function kebabToCamelCase() {
-  // your code here
+function kebabToCamelCase(inputStr) {
+    let resString = '';
+
+    if (inputStr) {
+        let matches = inputStr.split('-');
+
+        matches.map((element, index) => {
+
+            if (index > 0) {
+                element = element.charAt(0).toUpperCase() + element.slice(1);
+            }
+            resString += element;
+        });
+    } else {
+        new Error('incorrect input param');
+    }
+    return resString;
 }
 
 module.exports = kebabToCamelCase;
