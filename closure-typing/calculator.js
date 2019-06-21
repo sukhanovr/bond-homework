072f7fe7f5e8b44f4c7a942fcd1f8b97125a7746
Kateryna Mihal <Kateryna.Mihal@teaminternational.com>
2019-06-21 08:35:57 +0300

    test

--- closure-typing/calculator.js
+++ closure-typing/calculator.js
@@ -1,5 +1,26 @@
-function calculator() {
-  // your code here
+function calculator(inputExp) {
+
+    let reDecimal = new RegExp(/\d+(\.\d{0,2})?/);
+
+    if (reDecimal.test(inputExp)) {
+		
+        let mathOperator = inputExp.match(/[\+\-\/\*\(]/)[0];
+        let nums = inputExp.split(mathOperator, 2);
+		
+        if (mathOperator === '+') {
+            return +nums[0] + +nums[1];
+        } else if (mathOperator === '-') {
+            return +nums[0] - +nums[1];
+        } else if (mathOperator === '*') {
+            return +nums[0] * +nums[1];
+        } else if (mathOperator === '/') {
+            return +nums[0] / +nums[1];
+        } else {
+            return 'another math operations'
+        }
+    } else {
+        return 'incorrect expression';
+    }
 }
 
 module.exports = calculator;
